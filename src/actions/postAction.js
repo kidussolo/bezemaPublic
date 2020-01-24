8e2a69fc00f46395bb893ff854d92fff7ba6739a
kidus <kidussolomon2004@gmail.com>
2020-01-24 16:59:40 +0300

    added delete and like actions

--- src/actions/postAction.js
+++ src/actions/postAction.js
@@ -90,6 +90,19 @@ export const setPostLoading = () => {
   };
 };
 
+// Add Like
+export const addLike = id => dispatch => {
+  axios
+    .post(`/api/posts/like/${id}`)
+    .then(res => dispatch(getPosts()))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
 // Clear errors
 export const clearErrors = () => {
   return {
