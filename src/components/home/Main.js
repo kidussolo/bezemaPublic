5dbe826e0c37c27a8a756e98432531a42a083d68
kidus <kidussolomon2004@gmail.com>
2020-01-24 17:00:48 +0300

    main component with tab navigation

--- src/components/home/Main.js
+++ src/components/home/Main.js
@@ -1,8 +1,25 @@
 import React, { Component } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import Newpost from "../home/Newpost";
+import Posts from "../post/Posts";
 
 class Main extends Component {
   render() {
-    return <h1>the main page</h1>;
+    return (
+      <Tabs>
+        <TabList>
+          <Tab>Posts</Tab>
+          <Tab>Add New</Tab>
+        </TabList>
+        <TabPanel>
+          <Posts />
+        </TabPanel>
+        <TabPanel>
+          <Newpost />
+        </TabPanel>
+      </Tabs>
+    );
   }
 }
 
